b585e6f3e632a3de622ff3618dcfe382a3e9fa59
benwiz <bwisialowski@gmail.com>
2020-12-23 22:49:24 -0500

    actual link

--- 2d/index.js
+++ 2d/index.js
@@ -11,21 +11,8 @@ function CenterControl(controlDiv, map) {
   controlUI.style.fontSize = "22px";
   controlUI.title = "Click to view 3D version";
   controlUI.text = "3D";
+  controlUI.setAttribute("href", "..");
   controlDiv.appendChild(controlUI);
-  // // Set CSS for the control interior.
-  // const controlText = document.createElement("div");
-  // controlText.style.color = "rgb(25,25,25)";
-  // controlText.style.fontFamily = "Roboto,Arial,sans-serif";
-  // controlText.style.fontSize = "16px";
-  // controlText.style.lineHeight = "38px";
-  // controlText.style.paddingLeft = "5px";
-  // controlText.style.paddingRight = "5px";
-  // controlText.innerHTML = "Center Map";
-  // controlUI.appendChild(controlText);
-  // // Setup the click event listeners: simply set the map to Chicago.
-  // controlUI.addEventListener("click", () => {
-  //   map.setCenter(chicago);
-  // });
 }
 
 function createMarker(map, location, isLast) {

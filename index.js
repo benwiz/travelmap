4cb5b51709ba8e4cc10a03b2525289a7d43cdce2
benwiz <bwisialowski@gmail.com>
2020-12-23 19:18:36 -0500

    minimal globe

--- index.js
+++ index.js
@@ -3,53 +3,22 @@ if(!Detector.webgl){
   // actually, probably display a link stating lack of webgl
   Detector.addGetWebGLMessage();
 } else {
-
-  var years = ['1990','1995','2000'];
   var container = document.getElementById('container');
   var globe = new DAT.Globe(container, {imgDir: "assets/"});
-
-  var i, tweens = [];
-
-  var settime = function(globe, t) {
-    return function() {
-      new TWEEN.Tween(globe).to({time: t/years.length},500).easing(TWEEN.Easing.Cubic.EaseOut).start();
-      var y = document.getElementById('year'+years[t]);
-      if (y.getAttribute('class') === 'year active') {
-        return;
-      }
-      var yy = document.getElementsByClassName('year');
-      for(i=0; i<yy.length; i++) {
-        yy[i].setAttribute('class','year');
-      }
-      y.setAttribute('class', 'year active');
-    };
-  };
-
-  for(var i = 0; i<years.length; i++) {
-    var y = document.getElementById('year'+years[i]);
-    y.addEventListener('mouseover', settime(globe,i), false);
-  }
-
-  var xhr;
+  var parsedYaml = YAML.load("locations.yml");
+  var locations = parsedYaml.locations;
+  var datoms = locations.map(function (loc) {
+    return [loc.lat, loc.lng, 0.1];
+  });
+  globe.addData(datoms.flat(), {format: "magnitude",
+                                name: "travel",
+                                animated: true});
+  globe.createPoints();
   TWEEN.start();
-
-
-  xhr = new XMLHttpRequest();
-  xhr.open('GET', 'population909500.json', true);
-  xhr.onreadystatechange = function(e) {
-    if (xhr.readyState === 4) {
-      if (xhr.status === 200) {
-        var data = JSON.parse(xhr.responseText);
-        window.data = data;
-        for (i=0;i<data.length;i++) {
-          globe.addData(data[i][1], {format: 'magnitude', name: data[i][0], animated: true});
-        }
-        globe.createPoints();
-        settime(globe,0)();
-        globe.animate();
-        document.body.style.backgroundImage = 'none'; // remove loading
-      }
-    }
-  };
-  xhr.send(null);
+  new TWEEN.Tween(globe)
+    .to({time: 0}, 500)
+    .easing(TWEEN.Easing.Cubic.EaseOut)
+    .start();
+  globe.animate();
+  document.body.style.backgroundImage = "none"; // remove loading
 }
